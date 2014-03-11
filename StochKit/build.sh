1216290d24dad515e87b500a0f19b0d9a03d6bfd
choruk <chris@horukmail.com>
2014-03-10 17:31:55 -0700

    testing auto build of stochkit

---
+++ StochKit/build.sh
@@ -0,0 +1,3 @@
+./StochKit/install.sh 1>"build_stdout.log" 2>"build_stderr.log"
+echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
+source ~/.bashrc
---
+++ StochKit/test.sh
@@ -0,0 +1,1 @@
+"$STOCHKIT_HOME/ssa" -m "$STOCHKIT_HOME/models/examples/dimer_decay.xml" -r 1 -t 1 -i 1 --out-dir "$PWD/tmp" --force 1>"test_stdout.log" 2>"test_stderr.log"
