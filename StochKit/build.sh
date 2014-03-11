0e6aff286b37d92aa10877e12da53b816498cbc5
choruk <chris@horukmail.com>
2014-03-11 01:04:16 -0700

    update

--- StochKit/build.sh
+++ StochKit/build.sh
@@ -1,3 +1,5 @@
 ./StochKit/install.sh 1>"build_stdout.log" 2>"build_stderr.log"
 echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
+echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.profile
 source ~/.bashrc
+source ~/.profile
