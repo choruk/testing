17bc4b3ba5e6985eda5722c7908d7fa7ffaa2309
choruk <chris@horukmail.com>
2014-03-11 02:06:00 -0700

    update2

--- StochKit/build.sh
+++ StochKit/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ./StochKit/install.sh 1>"build_stdout.log" 2>"build_stderr.log"
 echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
 echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.profile
--- StochKit/test.sh
+++ StochKit/test.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 "$STOCHKIT_HOME/ssa" -m "$STOCHKIT_HOME/models/examples/dimer_decay.xml" -r 1 -t 1 -i 1 --out-dir "$PWD/tmp" --force 1>"test_stdout.log" 2>"test_stderr.log"
