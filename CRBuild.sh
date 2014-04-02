cc04889e9ca2c9c108a0a23e8c9918ba3224e327
choruk <chris@horukmail.com>
2014-04-01 19:25:59 -0700

    update build/test scripts

--- CRBuild.sh
+++ CRBuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./StochKit/install.sh 1>"build_stdout.log" 2>"build_stderr.log"
-echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
-echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.profile
-source ~/.bashrc
-source ~/.profile
+./StochKit/install.sh
+# echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
+# echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.profile
+# source ~/.bashrc
+# source ~/.profile
--- CRTest.sh
+++ CRTest.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-"$STOCHKIT_HOME/ssa" -m "$STOCHKIT_HOME/models/examples/dimer_decay.xml" -r 1 -t 1 -i 1 --out-dir "$PWD/tmp" --force 1>"test_stdout.log" 2>"test_stderr.log"
+export STOCHKIT_HOME=$PWD/StochKit
+export STH=$STOCHKIT_HOME
+mkdir -p /tmp/StochKit
+$STH/ssa -m $STH/models/examples/dimer_decay.xml -r 1 -t 1 -i 1 --out-dir /tmp/StochKit --force
+$STH/tau_leaping -m $STH/models/examples/dimer_decay.xml -r 1 -t 1 -i 1 --out-dir /tmp/StochKit --force
+rm -rf /tmp/StochKit
