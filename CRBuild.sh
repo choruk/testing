b412b4f6b7a1d4e203cbf0f1435bd50addfa1300
choruk <chris@horukmail.com>
2014-04-05 20:35:30 -0700

    update build script for infrastructures with apt package manager

--- CRBuild.sh
+++ CRBuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install libxml2-dev
 ./StochKit/install.sh
 # echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
 # echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.profile
