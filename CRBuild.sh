1740f0b5741b6e7737cee0629b834041ad9d8db1
choruk <chris@horukmail.com>
2014-04-05 21:47:46 -0700

    set yes flag on apt-get

--- CRBuild.sh
+++ CRBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install libxml2-dev
+sudo apt-get -y install libxml2-dev
 ./StochKit/install.sh
 # echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.bashrc
 # echo "export STOCHKIT_HOME=$PWD/StochKit" >> ~/.profile
