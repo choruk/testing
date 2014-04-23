c76ee3f717ac216e2e744fc5567ba408f3cc5e72
choruk <chris@horukmail.com>
2014-04-22 19:42:34 -0700

    if stochkit fails to build the test script could still succeed, this is no longer the case

--- CRTest.sh
+++ CRTest.sh
@@ -4,4 +4,4 @@ export STH=$STOCHKIT_HOME
 mkdir -p /tmp/StochKit
 $STH/ssa -m $STH/models/examples/dimer_decay.xml -r 1 -t 1 -i 1 --out-dir /tmp/StochKit --force
 $STH/tau_leaping -m $STH/models/examples/dimer_decay.xml -r 1 -t 1 -i 1 --out-dir /tmp/StochKit --force
-rm -rf /tmp/StochKit
+#rm -rf /tmp/StochKit
